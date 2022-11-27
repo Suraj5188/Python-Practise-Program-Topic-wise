ab97ca5e93bd766adecf2d8ae48d71ee739c7cad
Suraj Gaikwad <95357888+Suraj5188@users.noreply.github.com>
2022-11-27 12:18:40 +0530

    Add files via upload
    Variable and datatypes in python practise program

---
+++ Practise set 2.py
@@ -0,0 +1,43 @@
+#Practise Set
+
+#Q.1 Write a python program to add two numbers
+
+a=45
+b=55
+c=a+b
+print(c)
+
+#Q.2 Write a Python Programm Program to find remindersnwhen a number is divided by 2
+
+d=a%b
+print(d)
+
+#Q.3 Check the style of the variable assigned using input() function
+
+n1=input("Enter number you want:")
+print("You Entered :",n1)
+n1=int(n1)
+print("Type of Variable is",type(n1))
+
+# Q.4 Use comparison operator to find out wheather a given variable 'a' is greater than 'b' or not
+#Take a=34 & b=80
+
+a1=34
+b1=80
+print(a1>b1)
+print(b1>a1)
+
+# Q.5 Write a Python to find average of two numbers entered by the user
+
+num1=input("Enter 1st numbers:")
+num2=input("Enter 2nd numbers:")
+num1=int(num1)
+num2=int(num2)
+avg=(num1+num2)/2
+print(avg)
+
+#Q.6 Write a python program to calculate cube of number enterd by the user
+
+num3=input("Enter Number You Want:")
+num3=int(num3)
+print(num3*num3*num3)
