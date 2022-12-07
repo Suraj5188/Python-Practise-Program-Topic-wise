8ab6d45e825acaeeadbfd66bfdcf03ba8d031e9c
Suraj Gaikwad <95357888+Suraj5188@users.noreply.github.com>
2022-12-07 17:12:46 +0530

    Add files via upload
    Time Module

---
+++ module_time.py
@@ -0,0 +1,13 @@
+#module time 
+
+import time
+
+
+k=0
+while(k<10):
+    print("Hello suraj Good evening")
+    k+=1
+    print(time.time()-initial)
+
+for i in range(10):
+    print("Hello Suraj")
